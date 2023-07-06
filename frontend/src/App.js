e78cd4392031dfd499b162967906732e0ac2a279
Dhruv <dhruvnijhawann@gmail.com>
2023-07-06 12:39:23 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Canvas1 from './Component/Canvas1.js';
+
+function App() {
+  return (
+    <>
+      <Canvas1/>
+    </>
+  );
+}
+
+export default App;
